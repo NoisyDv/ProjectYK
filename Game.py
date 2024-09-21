5bd279879df8812dbe32efeaa880b6463ac239f6
NosiyDv <tnnaphwngwili@gmail.com>
2024-09-21 10:39:34 +0700

    deleted:haha.py

--- Game.py
+++ Game.py
@@ -5,12 +5,14 @@ class game:
         self.screen=pygame.display.set_mode((1000,800))
         self.clock=pygame.time.Clock()
         self.reg=pygame.rect.Rect(100,100,200,200)
+        self.hah=pygame.rect.Rect(200,200,10,10)
         
     def game_loop(self):
         while True:
-            
-            self.screen.fill((0,125,200))
+            self.screen.fill((0,125,150))
             pygame.draw.rect(self.screen,(250,10,20),self.reg)
+            pygame.draw.rect(self.screen,(200,200,200),self.hah)
+            pygame.draw.rect(self.screen,(20,0,0),self.hah)
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     pygame.quit()
